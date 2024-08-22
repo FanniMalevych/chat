1b2a0fe9758783211df378bb3cef5aae9e31ee20
fanni <fmalevych@zoolatech.com>
2024-08-22 10:33:40 +0300

    established db connection

--- server/server.js
+++ server/server.js
@@ -1,12 +1,18 @@
 import express  from 'express'
 import dotenv from 'dotenv'
+import connectToMongoDB from './db/connectToMongoDB.js'
+
+dotenv.config()
 
 const app = express()
-dotenv.config()
+
 const PORT = process.env.PORT || 6000
 
 app.get('/', (req, res) => {
     res.send('hi hi')
 })
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+app.listen(PORT, () => {
+    connectToMongoDB()
+    console.log(`Server is running on port ${PORT}`)
+})
